b504840ee47fa240bde136af13f0f46fb35ded2c
basileuszsr <zhang.shir@northeastern.edu>
2021-05-09 16:11:20 +0800

    5.9

---
+++ TwoPointers/FastSlowPointers/lc206/Reverse.java
@@ -0,0 +1,14 @@
+public class Reverse {
+    public ListNode reverse(ListNode head) {
+        if (head == null) return head;
+        ListNode current = head;
+        ListNode previous = null;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        return previous;
+    }
+}
