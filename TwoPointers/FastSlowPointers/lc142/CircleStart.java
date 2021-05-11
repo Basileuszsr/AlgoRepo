9a0f11991d245b781fadc1107dbda67be5a41028
basileuszsr <zhang.shir@northeastern.edu>
2021-05-11 17:04:33 +0800

    5.11

---
+++ TwoPointers/FastSlowPointers/lc142/CircleStart.java
@@ -0,0 +1,21 @@
+public class CircleStart {
+    public ListNode circleStart(ListNode head) {
+        if (head == null) return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (true) {
+            if (fast == null || fast.next == null) return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
