ffa95ddf78e7da07387398df6ff8c9e2e8a3e4bc
Basileuszsr <zhang.shir@northeastern.edu>
2021-07-28 22:27:10 +0800

    7.28 commit

--- Leetcode/69.sqrt-x.java
+++ Leetcode/69.sqrt-x.java
@@ -7,20 +7,19 @@
 // @lc code=start
 class Solution {
     public int mySqrt(int x) {
-        if (x <= 1) return x;
-        long left = 1;
-        long right = x / 2;
-        while (left <= right) {
-            long mid = left + (right - left) / 2;
-            if (mid * mid == x) {
-                return (int)mid;
-            } else if (mid * mid < x) {
-                left = mid + 1;
+        if (x == 0) return 0;
+        if (x == 1) return 1;
+        int left = 1;
+        int right = x / 2;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid > x / mid) {
+                right = mid;
             } else {
-                right = mid - 1;
+                left = mid + 1;
             }
         }
-        return (int)left;
+        return right - 1;
     }
 }
 // @lc code=end
---
+++ Leetcode/Amazon/572.subtree-of-another-tree.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=572 lang=java
+ *
+ * [572] Subtree of Another Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) return true;
+        if (root == null) return false;
+        return isTheSame(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isTheSame(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
+            return false;
+        }
+        if (root.val != subRoot.val) {
+            return false;
+        }
+        return isTheSame(root.left, subRoot.left) && isTheSame(root.right, subRoot.right);
+    }
+}
+// @lc code=end
+
